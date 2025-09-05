a1ac4d19790fdfa542b89392cf881c348ada8995
Void Defi <152207837+VoidDefi@users.noreply.github.com>
2025-09-05 23:06:44 +0300

    Update FilterItems view and add fire-rarity filter

--- Items/ItemFilterItem.cs
+++ Items/ItemFilterItem.cs
@@ -1,29 +1,43 @@
-﻿using Terraria;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using System;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.GameContent.UI;
 using Terraria.ID;
+using Terraria.Initializers;
 using Terraria.Localization;
 using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
 
 namespace MechTransfer.Items
 {
     [Autoload(false)]
     public class ItemFilterItem : ModItem
     {
-        public delegate bool MatchConditionn(Item item);
+        public override string Texture => (GetType().Namespace + "." + Name).Replace('.', '/');
+
+        public delegate bool MatchCondition(Item item);
 
         public int recipeItem = -1;
         public int Rarity = 0;
         public bool expert = false;
+        public bool master = false;
+        public Texture2D categoryTexture = null;
+        public float customCategoryScale = 0;
+        public string TextureName = "BaseFilterItem";
 
-        private MatchConditionn matchConditionn;
+        private MatchCondition matchCondition;
 
         protected override bool CloneNewInstances => true;
 
         public override string Name { get; }
 
-        public ItemFilterItem(string name, MatchConditionn matchConditionn)
+        public ItemFilterItem(string name, MatchCondition matchCondition)
         {
             Name = name;
-            this.matchConditionn = matchConditionn;
+            this.matchCondition = matchCondition;
         }
 
         public override LocalizedText DisplayName
@@ -44,11 +58,12 @@ namespace MechTransfer.Items
             Item.value = Item.buyPrice(0, 5, 0, 0);
             Item.rare = Rarity;
             Item.expert = expert;
+            Item.master = master;
         }
 
         public bool MatchesItem(Item item)
         {
-            return matchConditionn(item);
+            return matchCondition(item);
         }
 
         public override void AddRecipes()
@@ -61,6 +76,114 @@ namespace MechTransfer.Items
                 r.AddTile(TileID.WorkBenches);
                 r.Register();
             }
+        }
+
+        private void SetColor()
+        {
+            int rareType = Item.rare;
+            Color newColor = Color.White;
+            ModRarity modRare = RarityLoader.GetRarity(Rarity);
+
+            if (modRare != null)
+            {
+                newColor = modRare.RarityColor;
+            }
+
+            else if (expert)
+            {
+                newColor.R = (byte)Main.DiscoR;
+                newColor.G = (byte)Main.DiscoG;
+                newColor.B = (byte)Main.DiscoB;
+            }
+
+            else if (master)
+            {
+                float colorOffset = ((float)Main.mouseTextColor) / 255f;
+                newColor = new Color((byte)(255f * colorOffset), (int)(byte)(Main.masterColor * 200f * colorOffset), 0, 255);
+            }
+
+            else
+            {
+                newColor = ItemRarity.GetColor(rareType);
+            }
+
+            Item.color = newColor;
+        }
+
+        public void SetCategoryTexture(int type)
+        {
+            ModItem modItem = ModContent.GetModItem(type);
+            if (modItem != null)
+            {
+                SetCategoryTexture(modItem.Texture);
+            }
+
+            else
+            {
+                SetCategoryTexture($"Terraria/Images/Item_{type}");
+            }
+        }
+
+        public void SetCategoryTexture(string textureName)
+        {
+            if (textureName == null)
+                return;
+
+            try 
+            {
+                categoryTexture = ModContent.Request<Texture2D>(textureName, AssetRequestMode.ImmediateLoad).Value;
+            }
+
+            catch
+            {
+
+            }
+        }
+
+        public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            SetColor();
+
+            return false;
+        }
+
+        public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
+        {
+            SetColor();
+
+            return false;
+        }
+
+        private void Draw(SpriteBatch spriteBatch, Vector2 position, Color drawColor, Vector2 origin, float scale, float rotation)
+        {
+            Texture2D filterTexture = ModContent.Request<Texture2D>((GetType().Namespace + "." + TextureName).Replace('.', '/'), AssetRequestMode.ImmediateLoad).Value;
+            Color filterColor = new Color(((float)drawColor.R / 255f) * ((float)Item.color.R / 255f),
+                                          ((float)drawColor.G / 255f) * ((float)Item.color.G / 255f),
+                                          ((float)drawColor.B / 255f) * ((float)Item.color.B / 255f)); //If you use "itemColor", the item in the frame may flicker and behave strangely in low light.
+
+            spriteBatch.Draw(filterTexture, position, null, filterColor, rotation, origin, scale, 0, 0);
+
+            if (categoryTexture != null)
+            {
+                int categoryHeight = categoryTexture.Height;
+                int categoryWidth = categoryTexture.Width;
+
+                float drawOffset = 21 - 16;
+                float categoryScale = customCategoryScale == 0 ? scale * (26f / (float)Math.Max(categoryWidth, categoryHeight)) : scale * customCategoryScale;
+
+                spriteBatch.Draw(categoryTexture, position, null, drawColor, rotation, new Vector2(categoryWidth / 2 - (drawOffset), categoryHeight / 2 - (drawOffset)), categoryScale, 0, 0);
+            }
+        }
+
+        public override void PostDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            Draw(spriteBatch, position, drawColor, origin, scale, 0);
+        }
+
+        public override void PostDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, float rotation, float scale, int whoAmI)
+        {
+            Vector2 position = Item.position - Main.screenPosition + new Vector2(Item.width / 2 - 0.5f, Item.height / 2 - 0.5f);
+            Draw(spriteBatch, position, lightColor, new Vector2(Item.width / 2, Item.height / 2), scale, rotation);
         }
     }
 }
---
+++ TransferFilterTile.cs
@@ -0,0 +1,308 @@
+﻿using MechTransfer.Items;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using System;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace MechTransfer.Tiles
+{
+    [Autoload(false)]
+    public class TransferFilterTile : FilterableTile<TransferFilterTileEntity>, ITransferPassthrough
+    {
+        private Dictionary<int, ItemFilterItem> filterItems = new Dictionary<int, ItemFilterItem>();
+
+        private HashSet<int> Bags;
+
+        public override void PostSetDefaults()
+        {
+            AddMapEntry(MapColors.Passthrough, GetPlaceItem(0).DisplayName);
+
+            ModContent.GetInstance<TransferAgent>().passthroughs.Add(Type, this);
+            ModContent.GetInstance<TransferPipeTile>().connectedTiles.Add(Type);
+
+            base.PostSetDefaults();
+        }
+
+        protected override void SetTileObjectData()
+        {
+            TileObjectData.newTile.LavaDeath = false;
+            TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.None, 0, 0);
+            base.SetTileObjectData();
+        }
+
+        public bool ShouldPassthrough(Point16 location, Item item)
+        {
+            TransferFilterTileEntity TE;
+            if (TryGetEntity(location.X, location.Y, out TE))
+            {
+                ItemFilterItem filterItem;
+                if (filterItems.TryGetValue(TE.item.type, out filterItem))
+                {
+                    if (Main.tile[location.X, location.Y].TileFrameY == 0)
+                        return filterItem.MatchesItem(item);
+                    else
+                        return !filterItem.MatchesItem(item);
+                }
+                else
+                {
+                    if (Main.tile[location.X, location.Y].TileFrameY == 0)
+                        return TE.item.type == item.type;
+                    else
+                        return TE.item.type != item.type;
+                }
+            }
+            return false;
+        }
+
+        public override string HoverText(TransferFilterTileEntity entity)
+        {
+            Tile tile = Main.tile[entity.Position.X, entity.Position.Y];
+            if (tile.TileFrameY == 0)
+                return Language.GetTextValue("Mods.MechTransfer.UI.Hover.TransferFilterItem");
+            else
+                return Language.GetTextValue("Mods.MechTransfer.UI.Hover.InverseTransferFilterItem");
+        }
+
+        public override int GetDropKind(int Fx, int Fy)
+        {
+            return Fy / tileObjectData.CoordinateFullHeight;
+        }
+
+        public override void PostLoad()
+        {
+            PlaceItems = new ModItem[2];
+
+            //Filter
+            PlaceItems[0] = SimplePrototypeItem.MakePlaceable(Mod, "TransferFilterItem", Type, 16, 16, 0);
+
+            //InverseFilter
+            PlaceItems[1] = SimplePrototypeItem.MakePlaceable(Mod, "InverseTransferFilterItem", Type, 16, 16, 1);
+
+            LoadFilters();
+        }
+
+        public override void AddRecipes()
+        {
+            //Filter
+            Recipe r = Recipe.Create(PlaceItems[0].Item.type, 1);
+            r.AddIngredient(ModContent.ItemType<PneumaticActuatorItem>(), 1);
+            r.AddIngredient(ItemID.Actuator, 1);
+            r.AddIngredient(ItemID.ItemFrame, 1);
+            r.AddTile(TileID.WorkBenches);
+            r.Register();
+            Recipe r2 = Recipe.Create(PlaceItems[0].Item.type, 1);
+            r2.AddIngredient(PlaceItems[1]);
+            r2.Register();
+
+            //InverseFilter
+            r = Recipe.Create(PlaceItems[1].Item.type, 1);
+            r.AddIngredient(ModContent.ItemType<PneumaticActuatorItem>(), 1);
+            r.AddIngredient(ItemID.Actuator, 1);
+            r.AddIngredient(ItemID.ItemFrame, 1);
+            r.AddTile(TileID.WorkBenches);
+            r.Register();
+            r2 = Recipe.Create(PlaceItems[1].Item.type, 1);
+            r2.AddIngredient(PlaceItems[0]);
+            r2.Register();
+
+            LoadBagFilter();
+
+            string logFilterTestsEnvVar = Environment.GetEnvironmentVariable("MECHTRANSFER_LOGFILTERTESTS");
+            if (logFilterTestsEnvVar != null && logFilterTestsEnvVar.Equals("TRUE", StringComparison.OrdinalIgnoreCase))
+                LogFilterTests();
+        }
+
+        private ItemFilterItem createFilter(string type, int recipeItem, ItemFilterItem.MatchCondition condition, bool categoryTexture = true, int otherCategoryTexture = -1)
+        {
+            ItemFilterItem filter = new ItemFilterItem(type + "FilterItem", condition);
+            filter.recipeItem = recipeItem;
+
+            if (categoryTexture)
+            {
+                if (otherCategoryTexture > -1)
+                {
+                    filter.SetCategoryTexture(otherCategoryTexture);
+                }
+
+                else if (recipeItem > -1)
+                {
+                    filter.SetCategoryTexture(recipeItem);
+                }
+            }
+
+            Mod.AddContent(filter);
+
+            //Nebula: Easter egg handling is now done at runtime by ItemFilterItem.DisplayName itself
+            filterItems.Add(filter.Item.type, filter);
+
+            return filter;
+        }
+
+        private void LoadFilters()
+        {
+            string iconsPath = "MechTransfer/Items/Icons/";
+
+            //Main
+
+            createFilter("Any", -1, x => true).TextureName = "AnyFilter";
+
+            //Rarities
+
+            createFilter("Rarity-Gray", ItemID.GrayPaint, x => x.rare == -1, false).Rarity = -1;
+            createFilter("Rarity-White", ItemID.WhitePaint, x => x.rare == 0, false).Rarity = 0;
+            createFilter("Rarity-Blue", ItemID.BluePaint, x => x.rare == 1, false).Rarity = 1;
+            createFilter("Rarity-Green", ItemID.GreenPaint, x => x.rare == 2, false).Rarity = 2;
+            createFilter("Rarity-Orange", ItemID.OrangePaint, x => x.rare == 3, false).Rarity = 3;
+            createFilter("Rarity-LightRed", ItemID.RedPaint, x => x.rare == 4, false).Rarity = 4;
+            createFilter("Rarity-Pink", ItemID.PinkPaint, x => x.rare == 5, false).Rarity = 5;
+            createFilter("Rarity-LightPurple", ItemID.PurplePaint, x => x.rare == 6, false).Rarity = 6;
+            createFilter("Rarity-Lime", ItemID.LimePaint, x => x.rare == 7, false).Rarity = 7;
+            createFilter("Rarity-Yellow", ItemID.YellowPaint, x => x.rare == 8, false).Rarity = 8;
+            createFilter("Rarity-Cyan", ItemID.CyanPaint, x => x.rare == 9, false).Rarity = 9;
+            createFilter("Rarity-Red", ItemID.RedPaint, x => x.rare == 10, false).Rarity = 10;
+            createFilter("Rarity-Purple", ItemID.PurplePaint, x => x.rare == 11, false).Rarity = 11;
+            createFilter("Rarity-Rainbow", ItemID.DemonHeart, x => x.expert == true, false).expert = true;
+            createFilter("Rarity-FieryRed", 4924, x => x.master == true, false).master = true;
+            createFilter("Rarity-Amber", ItemID.Amber, x => x.rare == -11, false).Rarity = -11;
+
+            //Equipment
+
+            createFilter("Equipable", ItemID.Shackle, x => (x.headSlot >= 0 || x.bodySlot >= 0 || x.legSlot >= 0 || x.accessory || Main.projHook[x.shoot] || x.mountType >= 0 || x.dye > 0 || (x.buffType > 0 && (Main.lightPet[x.buffType] || Main.vanityPet[x.buffType]))), false).SetCategoryTexture(iconsPath + "EquipmentIcon");
+            createFilter("Armor", ItemID.WoodBreastplate, x => ((x.headSlot >= 0 || x.bodySlot >= 0 || x.legSlot >= 0) && !x.vanity));
+            createFilter("Vanity", ItemID.FamiliarWig, x => (x.vanity));
+            createFilter("Accessory", ItemID.Shackle, x => (x.accessory)).customCategoryScale = 0.8f;
+            Main.checkHalloween();
+            createFilter("Dye", ItemID.SilverDye, x => (x.dye > 0)).customCategoryScale = 0.7f;
+
+            //One use
+
+            createFilter("Ammo", ItemID.MusketBall, x => x.ammo != 0).customCategoryScale = 1f;
+            createFilter("Bait", ItemID.ApprenticeBait, x => x.bait > 0).customCategoryScale = 1f;
+            createFilter("Money", ItemID.GoldCoin, x => x.type == ItemID.CopperCoin || x.type == ItemID.SilverCoin || x.type == ItemID.GoldCoin || x.type == ItemID.PlatinumCoin).customCategoryScale = 1;
+            createFilter("Bag", ItemID.WoodenCrate, x => Bags.Contains(x.type)).customCategoryScale = 0.5f;
+
+            //Many use 
+
+            createFilter("Tool", ItemID.CopperPickaxe, x => x.pick > 0 || x.axe > 0 || x.hammer > 0, false).SetCategoryTexture(iconsPath + "ToolIcon");
+            createFilter("Weapon", ItemID.CopperShortsword, x => x.damage > 0 && x.pick == 0 && x.axe == 0 && x.hammer == 0, false).SetCategoryTexture(iconsPath + "WeaponIcon");
+
+            //Weapons
+
+            createFilter("Melee", ItemID.CopperShortsword, x => x.DamageType == DamageClass.Melee);
+            createFilter("Magic", ItemID.LesserManaPotion, x => x.DamageType == DamageClass.Magic, true, ItemID.AmethystStaff);
+            createFilter("Ranged", ItemID.WoodenBow, x => x.DamageType == DamageClass.Ranged);
+            createFilter("Summon", ItemID.SummoningPotion, x => x.DamageType == DamageClass.Summon, true, ItemID.SlimeStaff);
+            createFilter("Thrown", ItemID.Shuriken, x => x.DamageType == DamageClass.Throwing).customCategoryScale = 1;
+
+            //Consumes
+
+            createFilter("Consumable", ItemID.PumpkinPie, x => x.consumable, true, ItemID.Glowstick).customCategoryScale = 0.8f;
+            createFilter("Material", ItemID.Wood, x => x.material).customCategoryScale = 0.7f;
+            createFilter("Potion", ItemID.LesserHealingPotion, x => x.consumable && (x.healLife > 0 || x.healMana > 0 || x.buffType > 0)).customCategoryScale = 0.8f;
+
+            //Places
+
+            createFilter("Tile", ItemID.DirtBlock, x => x.createTile > -1).customCategoryScale = 0.8f;
+            createFilter("Wall", ItemID.WoodWall, x => x.createWall > 0).customCategoryScale = 0.5f;
+        }
+
+        private void LoadBagFilter()
+        {
+            Bags =
+            [
+                ItemID.HerbBag,
+                ItemID.CanOfWorms,
+                ItemID.Oyster,
+
+                ItemID.GoodieBag,
+                ItemID.Present,
+                ItemID.BluePresent,
+                ItemID.GreenPresent,
+                ItemID.YellowPresent,
+
+                ItemID.KingSlimeBossBag,
+                ItemID.EyeOfCthulhuBossBag,
+                ItemID.EaterOfWorldsBossBag,
+                ItemID.BrainOfCthulhuBossBag,
+                ItemID.QueenBeeBossBag,
+                ItemID.DeerclopsBossBag,
+                ItemID.WallOfFleshBossBag,
+                ItemID.SkeletronBossBag,
+                ItemID.DestroyerBossBag,
+                ItemID.TwinsBossBag,
+                ItemID.SkeletronPrimeBossBag,
+                ItemID.PlanteraBossBag,
+                ItemID.GolemBossBag,
+                ItemID.FishronBossBag,
+                ItemID.CultistBossBag,
+                ItemID.MoonLordBossBag,
+                ItemID.BossBagBetsy,
+                ItemID.FairyQueenBossBag,
+                ItemID.QueenSlimeBossBag,
+                ItemID.BossBagDarkMage,
+                ItemID.BossBagOgre,
+
+                ItemID.LockBox,
+                ItemID.ObsidianLockbox,
+                ItemID.WoodenCrate,
+                ItemID.IronCrate,
+                ItemID.GoldenCrate,
+                ItemID.JungleFishingCrate,
+                ItemID.FloatingIslandFishingCrate,
+                ItemID.CorruptFishingCrate,
+                ItemID.CrimsonFishingCrate,
+                ItemID.HallowedFishingCrate,
+                ItemID.DungeonFishingCrate,
+                ItemID.FrozenCrate,
+                ItemID.OasisCrate,
+                ItemID.LavaCrate,
+                ItemID.OceanCrate,
+                ItemID.WoodenCrateHard,
+                ItemID.IronCrateHard,
+                ItemID.GoldenCrateHard,
+                ItemID.JungleFishingCrateHard,
+                ItemID.FloatingIslandFishingCrateHard,
+                ItemID.CorruptFishingCrateHard,
+                ItemID.CrimsonFishingCrateHard,
+                ItemID.HallowedFishingCrateHard,
+                ItemID.DungeonFishingCrateHard,
+                ItemID.FrozenCrateHard,
+                ItemID.OasisCrateHard,
+                ItemID.LavaCrateHard,
+                ItemID.OceanCrateHard
+            ];
+
+            for (int i = 0; i < ItemLoader.ItemCount; i++)
+            {
+                ModItem item = ItemLoader.GetItem(i);
+                if (item != null && item.GetType().GetMethod("ModifyItemLoot").DeclaringType != typeof(ModItem))
+                {
+                    Bags.Add(i);
+                }
+            }
+        }
+
+        private void LogFilterTests()
+        {
+            Mod.Logger.Debug("---BEGIN FILTER LISTING---");
+            foreach (var item in filterItems)
+            {
+				Mod.Logger.Debug("----" + item.Value.Name);
+                foreach (Item testItem in ContentSamples.ItemsByType.Values)
+                {
+                    if (item.Value.MatchesItem(testItem))
+						Mod.Logger.Debug(testItem.Name);
+                }
+            }
+			Mod.Logger.Debug("---END FILTER LISTING---");
+        }
+    }
+}
